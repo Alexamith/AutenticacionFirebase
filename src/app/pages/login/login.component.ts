bba32c2d1c1a794ad19272b0957632a08d6ed155
Alexamith <samirportillo231099@gmail.com>
2021-03-25 11:56:15 -0600

    Validacion formulario login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserModel } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  user: UserModel;
+  
   constructor() { }
 
   ngOnInit() {
+    this.user = new UserModel();
   }
+  submitFormLogin(formLogin:NgForm){
+    if (formLogin.invalid) {
+      return;
+    }
+    console.log(formLogin);
+    console.log('Formulario login');
 
+  }
 }
